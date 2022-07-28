c2eb9e75fe2cccf80b1306a1cb702a4fa1830f67
jiangyeka <940495614@qq.com>
2022-07-28 08:40:42 +0800

    feat -m 一部分进度

--- README.md
+++ README.md
@@ -25,4 +25,48 @@ qs
 26. 全局上下文
 27. continue break
 29. object的键值自动转换字符串，因此如果传入obj，会a["[object Object]"]变成这样，这也算隐式类型转换的一种
+
+
 ✅
+43. ...'string'
+
+
+
+79. for in for of 
+---
+80. array的项可以使任意值包括null和undefined
+81. 函数具备形参，但是未传入实参，那么参数的值就变为undefined
+    
+82. this的指向取决于他的位置？？
+
+
+85. 链式调用的.then res=>res 为前一个回调函数返回的值
+86. ？？
+
+88. 函数的形参可以进行依赖设置，前提是只能前面的依赖后面的，否则会依法referenceError
+89. export 和 import * as ??
+    
+91. push的返回值，引申所有数组的方法的返回值是什么
+92. 箭头函数的实例没有其?? 查看__prototype prototype
+93. 在函数形参里面使用rest运算符，剩余参数的值只能是一个包含所有剩余参数的数组，并且只能作为最后一个参数
+    
+95. return自动插入，throw，break这样的关键字
+
+97. symbol不可枚举？？
+
+语法 syntax 多出现于符号
+类型 type 没有放到合适的位置
+引用 refer 未出现的关键字或者变量
+
+100. []&& ''&&  ||
+
+
+
+151. object对象里面的箭头函数的this指向什么
+152. 看不懂题目
+153. 什么b写法 forEntries A: entries
+B: values
+C: fromEntries
+D: forEach
+154. ？{} ？[]truthy 以及邮箱的正则
+155. !的作用，最后的返回值
--- main.js
+++ main.js
@@ -1,3 +1,52 @@
-let a=[]
-a['x']=1
-console.log(a);
+// var status = "😎"
+
+// setTimeout(() => {
+//   const status = "😍"
+
+//   const data = {
+//     status: "🥑",
+//     getStatus() {
+//       return this.status
+//     }
+//   }
+
+//   console.log(data.getStatus())
+//   console.log(data.getStatus.call(this))
+// }, 0)
+
+
+
+// function getName(name) {
+//     const hasName = //
+//   }
+
+
+// const colorConfig = {
+//     red: true,
+//     blue: false,
+//     green: true,
+//     black: true,
+//     yellow: false,
+//   }
+  
+//   const colors = ["pink", "red", "blue"]
+  
+//   console.log(colorConfig.red)
+
+
+//   const info = {
+//     [Symbol('a')]: 'b'
+//   }
+  
+//   console.log(info)
+//   console.log(Object.keys(info))
+// 🎉✨ This is my 100th question! ✨🎉
+// const keys = ["name", "age"]
+// const values = ["Lydia", 22]
+
+// let b=Object['fromEntries'](keys.map((_, i) => {
+// 	return [keys[i], values[i]]
+// })) 
+// console.log(b);
+
+console.log([...'helllo']);
