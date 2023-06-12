2029a0a6b93fb40e93acd4b347b04eabe2f424ee
jiangyeka <940495614@qq.com>
2023-06-12 21:16:48 +0800

    feat: 145

---
+++ 145.二叉树的后序遍历.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const postorderTraversal = function (root) {
+  const ans = []
+  const dfs = (root) => {
+    if (!root)
+      return
+    dfs(root.left)
+    dfs(root.right)
+    ans.push(root.val)
+  }
+  dfs(root)
+
+  return ans
+}
+// @lc code=end
