9e9f02939b8740e3171412c8d966f631977172a5
jiangyeka <940495614@qq.com>
2023-06-18 18:12:25 +0800

    feat: 54

---
+++ all/1-100/54.螺旋矩阵.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+const spiralOrder = function (matrix) {
+  const m = matrix.length
+  const n = matrix[0].length
+  const res = []
+  if (m === 1 || n === 1)
+    return matrix.flat(1)
+
+  const loopNumber = Math.min(m, n) / 2
+  for (let x = 0; x < loopNumber; x++) {
+    // 起点
+    for (let i = x, j = x; j < n - x; j++)
+      res.push(matrix[i][j])
+
+    for (let i = x + 1, j = n - x - 1; i < m - x; i++)
+      res.push(matrix[i][j])
+
+    if (x !== m - x - 1 && n - x - 1 !== x) {
+      for (let i = m - x - 1, j = n - x - 1 - 1; j >= x; j--)
+        res.push(matrix[i][j])
+
+      for (let i = m - x - 1 - 1, j = x; i >= x + 1; i--)
+        res.push(matrix[i][j])
+    }
+  }
+
+  return res
+}
+// 模拟值得考虑的是极限情况如何push ，也就是只有一行或者一列的时候该如何去考虑
